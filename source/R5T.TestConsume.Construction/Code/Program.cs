f763b53c04d40edef3945bb8370b01ea2dd6856f
MinexAutomation <minex.automation@gmail.com>
2019-06-05 12:28:48 -0400

    Initial commit of source-stub.

---
+++ source/R5T.TestConsume.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.TestConsume.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
